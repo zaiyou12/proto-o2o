ab25d82e3f7368ec4873038082f8fa085afb3e30
aaron.so <zaiyou12@gmail.com>
2021-07-17 16:49:23 +0900

    ➕ Add vue router implement homepage

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <RouterView />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+<script lang="ts" setup>
+import Home from './pages/Home.vue'
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home-page">
+    <div class="banner">
+      <div class="container">
+        <h1>Proto-O2O-platform</h1>
+      </div>
+      <p>A place to demo our prototype.</p>
+    </div>
+  </div>
+</template>
